6d25028c8c6d42ff3d9d6726bfad2281dc42e7d9
Austin <aya57@sfu.ca>
2023-11-11 18:44:23 -0800

    account viewmodel

--- app/src/main/java/com/example/checklist/DatabaseViewModel.kt
+++ app/src/main/java/com/example/checklist/DatabaseViewModel.kt
@@ -5,7 +5,21 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.annotation.WorkerThread
 import java.lang.IllegalArgumentException
-class DatabaseViewModel {
-}
+class AccountViewModel(private val repository: AccountRepository) {
+    val allEntriesLiveData : LiveData<List<AccountInfo>> = repository.allAccounts.asLiveData()
+
+    fun insert(account : AccountInfo) {
+        repository.insert(account)
+    }
+
+    fun deleteEntry(pos : Int) {
+        val accountList = allEntriesLiveData.value
+        if (accountList != null && accountList.size > 0) {
+            val id = accountList[pos].id
+            repository.delete(id)
+        }
+    }
+    }
+
 
 
