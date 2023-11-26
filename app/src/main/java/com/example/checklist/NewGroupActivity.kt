ccfd9beb069a17c551da79d702fb6244a84fb6bc
Justin Y <justinyan050@gmail.com>
2023-11-25 17:16:58 -0800

    added checks in groups

--- app/src/main/java/com/example/checklist/NewGroupActivity.kt
+++ app/src/main/java/com/example/checklist/NewGroupActivity.kt
@@ -3,7 +3,10 @@ package com.example.checklist
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
 
 class NewGroupActivity : AppCompatActivity() {
 
@@ -17,18 +20,58 @@ class NewGroupActivity : AppCompatActivity() {
         database = Database()
         username = intent.getStringExtra("user")!!
 
+        val context = this
+
         findViewById<Button>(R.id.buttonJoinGroup).setOnClickListener {
             val groupID = findViewById<EditText>(R.id.inputJoinGroup).text.toString()
-            database.writeGroupParticipant(groupID, username)
-            finish()
+
+            lifecycleScope.launch {
+                var currentGroups = database.getGroupList()
+
+                kotlinx.coroutines.delay(200)
+
+                var ifExists = false
+
+                for (group in currentGroups) {
+                    if (group.groupId == groupID) {
+                        ifExists = true
+                    }
+                }
+
+                if (ifExists) {
+                    database.writeGroupParticipant(groupID, username)
+                    finish()
+                } else {
+                    Toast.makeText(context, "Error: Group doesn't Exist", Toast.LENGTH_LONG).show()
+                }
+            }
         }
 
         findViewById<Button>(R.id.buttonCreateGroup).setOnClickListener {
             val groupName = findViewById<EditText>(R.id.inputGroupName).text.toString()
             val groupDesc = findViewById<EditText>(R.id.inputGroupDesc).text.toString()
-            database.writeNewGroup(groupName, groupDesc)
-            database.writeGroupParticipant(groupName, username)
-            finish()
+
+            lifecycleScope.launch {
+                var currentGroups = database.getGroupList()
+
+                kotlinx.coroutines.delay(200)
+
+                var isUnique = true
+
+                for (group in currentGroups) {
+                    if (group.groupId == groupName) {
+                        isUnique = false
+                    }
+                }
+
+                if (isUnique) {
+                    database.writeNewGroup(groupName, groupDesc)
+                    database.writeGroupParticipant(groupName, username)
+                    finish()
+                } else {
+                    Toast.makeText(context, "Error: Group already Exists", Toast.LENGTH_LONG).show()
+                }
+            }
         }
 
         findViewById<Button>(R.id.buttonCancelGroup).setOnClickListener {
